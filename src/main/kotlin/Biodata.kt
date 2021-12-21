982d4c322e95bcc37ddb95d97f885d079125f6dc
unknown <sakinahumi1924@gmail.com>
2021-12-21 16:09:21 +0700

    first commit

---
+++ src/main/kotlin/Biodata.kt
@@ -0,0 +1,12 @@
+fun main() {
+    var Nama = "Sakinah Umi Pramita"
+    var NIM = "21090087"
+    var TTL = "Tegal,19 july 2003"
+    var Kelas = "1D"
+    var Prodi = "Teknik Informatika"
+    var datadiri = "$Nama $NIM $TTL $Kelas $Prodi"
+
+    println(datadiri)
+
+
+}
