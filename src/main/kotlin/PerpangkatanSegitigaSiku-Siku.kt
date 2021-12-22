762cae9771382499df0a3bcfdc6515fc304675b9
SakinahMita <sakinahumi1924@gmail.com>
2021-12-22 08:20:25 +0700

    *

---
+++ src/main/kotlin/PerpangkatanSegitigaSiku-Siku.kt
@@ -0,0 +1,15 @@
+fun main(){
+     repeat(1){
+         println("1")
+     }
+
+    repeat(1){
+        println("1 2")
+    }
+    repeat(1){
+        println("1 2 3")
+    }
+    repeat(1){
+        println("1 2 3 4")
+    }
+}
---
+++ src/main/kotlin/perpangkatan.kt
@@ -0,0 +1,11 @@
+fun main(){
+    var pangkat = "6 ^ 2"
+    println("pangkat : $pangkat")
+    var hasil1 = "36"
+    println(hasil1)
+
+    var pangkat1 = "4 ^ 2"
+    println("pangkat : $pangkat1")
+    var hasil = "16"
+    println(hasil)
+}
